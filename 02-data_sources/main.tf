764b40296de766c34542359114244fcb33745382
user <namanshetty91132@gmail.com>
2023-05-17 18:36:47 +0100

    Naman

--- 02-data_sources/main.tf
+++ 02-data_sources/main.tf
@@ -1,8 +1,14 @@
-data "aws_security_group" "selected" {
-  name = "allow-all"
+//data "aws_security_group" "selected" {
+//  name = "allow-all"
+//}
+
+//output "security_group_id" {
+//  value = data.aws_security_group.selected.id
+//}
+##above command prints security id of allow_all security group
+
+data "aws_security_group" "test"{}
+outtput "all_sg" {
+  value = data.aws_security_groups.test
 }
 
-output "security_group_id" {
-  value = data.aws_security_group.selected.id
-}
-##above command prints security id of allow_all security group
