43c6fd53426a7ce108a96d6f2f6232298b4c060a
user <namanshetty91132@gmail.com>
2023-05-19 08:30:57 +0100

    Naman

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -1,9 +1,10 @@
 resource "null_resource" "fruits" {
   //count = length(var.fruits)
+  for each = var.fruits
   provisioner "local-exec" {
      //command = "echo Fruit Name - ${var.fruits[count.index]}"
      //provisioner is another block which u can run in any resource after creating resource u wanted to deal with that u can go with provisioner
-     command = "echo  ${length(var.fruits)}"
+     command = "echo Fruit Name -  ${length(var.fruits[each.key])} - ${length(var.fruits[each.value])}"
   }
 }
 
