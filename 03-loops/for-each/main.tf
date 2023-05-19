eed2555487a4bc358cbf6072d81eb92024d92a75
user <namanshetty91132@gmail.com>
2023-05-19 08:49:42 +0100

    Naman

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -4,27 +4,35 @@ resource "null_resource" "fruits" {
   provisioner "local-exec" {
      //command = "echo Fruit Name - ${var.fruits[count.index]}"
      //provisioner is another block which u can run in any resource after creating resource u wanted to deal with that u can go with provisioner
-     command = "echo Fruit Name -  ${var.fruits[each.value["name"])}
+     command = "echo Fruit Name -  ${each.key}
   }
 }
 
 variable "fruits" {
   default = {
-    apple {
-      name = "apple"
-      count = 10
+    apple = 10
+    banana = 100
+    oranges = 200
     }
-    orange {
-      name = "orange"
-      count = 200
-    }
+ }
 
-    banana {
-      name = "banana"
-      count = 10
-    }
-  }
-}
+//variable "fruits" {
+//  default = {
+//    apple {
+//      name = "apple"
+//      count = 10
+//    }
+//    orange {
+//      name = "orange"
+//      count = 200
+//    }
+
+//    banana {
+//      name = "banana"
+//      count = 10
+//    }
+//  }
+//}
 
 // for above command row 7-11  3 fruits are there so 3 times its running but it wont show fruit names u can check by running
 
