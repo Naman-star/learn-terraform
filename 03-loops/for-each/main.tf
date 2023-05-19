fb43dc372ecd2204960cb10e91e70a67670a24d5
user <namanshetty91132@gmail.com>
2023-05-19 14:01:23 +0100

    Naman

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -7,6 +7,15 @@ resource "null_resource" "fruits" {
   }
 }
 
+esource "null_resource" "fruits1" {
+
+  for_each = var.fruits
+  provisioner "local-exec" {
+
+     command = "echo Fruit Name -  ${each.key} - ${each.value["count"]}"
+  }
+}
+
 variable "fruits" {
   default = {
     apple = 10
@@ -14,4 +23,23 @@ variable "fruits" {
     oranges = 200
     }
  }
+
+
+variable "fruits1" {
+  default = {
+    apple = {
+      name = "apple"
+      count = 10
+    }
+   oranges = {
+         name = "oranges"
+         count = 200
+       }
+       banana = {
+             name = "banana"
+             count = 100
+           }
+ }
+
+
 
