9846c140a2ffab710ec9d8cd21d37d739ed4624f
user <namanshetty91132@gmail.com>
2023-05-19 08:26:59 +0100

    Naman

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "fruits" {
   provisioner "local-exec" {
      //command = "echo Fruit Name - ${var.fruits[count.index]}"
      //provisioner is another block which u can run in any resource after creating resource u wanted to deal with that u can go with provisioner
-     command -= "echo  ${length(var.fruits)}"
+     command = "echo  ${length(var.fruits)}"
   }
 }
 
