bb453d91e9ff4d0367b3ec50d43c0f7f33b63d26
user <namanshetty91132@gmail.com>
2023-05-22 18:14:03 +0100

    Naman

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -6,3 +6,29 @@ variable "sample" {
 output "sample" {
   value = upper(var.sample)
 }
+
+
+variable "sample1" {
+  default = [ "abc" , "xyz"]
+}
+
+variable "sample2" {
+  default = {
+    xyz = 100
+    abc = 200
+
+  }
+}
+
+#print first value line var.sample[0] with a function.sample1
+
+output "sample1" {
+  value = element(var.sample1, 0)
+}
+
+output "sample2" {
+  value = lookup(var.sample2, "a1", "dummy")
+}
+
+
+
